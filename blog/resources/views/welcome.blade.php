78c28bcb38621001f6609e666fcdaac1fd15eeee
Yevgeniy <e@mpleev.com>
2019-11-22 16:48:16 +0300

    fix

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Ampleev Yevgeniy. Personal blog.</title>
+    <title>Yevgeniy Ampleev. IT Agile coach, scrum master</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Ampleev Yevgeniy
+            Евгений Амплеев
         </div>
 
         <div class="links">
