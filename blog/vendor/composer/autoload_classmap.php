b6dbc3d970c983b2e90247806cd83dc252b9a93c
Yevgeniy <e@mpleev.com>
2019-12-11 14:42:15 +0300

    fix

--- blog/vendor/composer/autoload_classmap.php
+++ blog/vendor/composer/autoload_classmap.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'App\\Article' => $baseDir . '/app/Article.php',
     'App\\BlogSection' => $baseDir . '/app/BlogSection.php',
+    'App\\Comment' => $baseDir . '/app/Comment.php',
     'App\\Console\\Kernel' => $baseDir . '/app/Console/Kernel.php',
     'App\\Exceptions\\Handler' => $baseDir . '/app/Exceptions/Handler.php',
     'App\\Http\\Controllers\\Auth\\ConfirmPasswordController' => $baseDir . '/app/Http/Controllers/Auth/ConfirmPasswordController.php',
