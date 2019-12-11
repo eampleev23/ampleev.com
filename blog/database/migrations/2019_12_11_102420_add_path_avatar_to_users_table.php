e5d8b00cd441f92508aa0134e5b7e9612878e010
Yevgeniy <e@mpleev.com>
2019-12-11 16:51:51 +0300

    fix

--- blog/database/migrations/2019_12_11_102420_add_path_avatar_to_users_table.php
+++ blog/database/migrations/2019_12_11_102420_add_path_avatar_to_users_table.php
@@ -14,7 +14,7 @@ class AddPathAvatarToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->char('avatar_path', 200);
+            $table->char('avatar_path', 200)->nullable();
         });
     }
 
