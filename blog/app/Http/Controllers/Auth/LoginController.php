76c36a270485b2312fad47a1d78711f9f7cf3a4d
Yevgeniy <e@mpleev.com>
2019-12-12 11:32:48 +0300

    fix

--- blog/app/Http/Controllers/Auth/LoginController.php
+++ blog/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Redirect;
 
 class LoginController extends Controller
 {
@@ -20,12 +21,18 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+//    /**
+//     * Where to redirect users after login.
+//     *
+//     * @var string
+//     */
+//    protected $redirectTo = '/home';
+
+    protected function redirectTo()
+    {
+//        return url('/profile/',auth()->user()->id);
+        return Redirect::back();
+    }
 
     /**
      * Create a new controller instance.
