5bc128ee2668dc0a2dc8ea321ecc36e4232bcf75
Yevgeniy <e@mpleev.com>
2019-12-12 16:10:32 +0300

    fix

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -20,17 +20,17 @@ Route::group([
     'middleware' => 'auth',
 ], function () {
 
-    Route::group([
-        'as' => 'blog.'
-    ],
-        function () {
-            Route::get('/blog', 'BlogController@show_old')->name('home');
-            Route::get('/blog-article', 'BlogController@show_article_test')->name('article_test');
-            Route::get('/article-{article_id}', 'BlogController@show_article')->name('show_article');
-        }
-    );
-
 });
+
+Route::group([
+    'as' => 'blog.'
+],
+    function () {
+        Route::get('/blog', 'BlogController@show_old')->name('home');
+        Route::get('/blog-article', 'BlogController@show_article_test')->name('article_test');
+        Route::get('/article-{article_id}', 'BlogController@show_article')->name('show_article');
+    }
+);
 
 
 Route::get('/', 'IndexController@show')->name('main');
