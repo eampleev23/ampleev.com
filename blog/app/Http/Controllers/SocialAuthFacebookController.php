12fd83879507b5ebc3e8402289f2d43fd6479e2f
Yevgeniy <e@mpleev.com>
2019-12-15 18:20:19 +0300

    fix

--- blog/app/Http/Controllers/SocialAuthFacebookController.php
+++ blog/app/Http/Controllers/SocialAuthFacebookController.php
@@ -16,8 +16,9 @@ class SocialAuthFacebookController extends Controller
      */
     public function redirect()
     {
-        $uri_for_back = $_SERVER['HTTP_REFERER'];
-        return Socialite::driver('facebook')->redirect($uri_for_back);
+//        $uri_for_back = $_SERVER['HTTP_REFERER'];
+//        return Socialite::driver('facebook')->redirect($uri_for_back);
+        return Socialite::driver('facebook')->redirect();
     }
 
     /**
