85b4b6cef2a404b64dc49c604f0ca51a50ad50fc
Yevgeniy <e@mpleev.com>
2019-12-15 20:24:35 +0300

    fix

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -54,24 +54,6 @@
 
 
 <body>
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : '456217141958876',
-            xfbml      : true,
-            version    : 'v5.0'
-        });
-        FB.AppEvents.logPageView();
-    };
-
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
 @section('sidebar')
     <div class="loader">
         <div class="loading-animation"></div>
