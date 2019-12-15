def045b86f201d362bf628c67adff2ca550ca0d4
Yevgeniy <e@mpleev.com>
2019-12-15 20:23:28 +0300

    fix

--- blog/resources/views/blog/article.blade.php
+++ blog/resources/views/blog/article.blade.php
@@ -92,19 +92,19 @@
                 <div class="col-xl-7 col-lg-8 col-md-10">
                     <hr>
                     <div class="d-flex align-items-center">
-{{--                        <span class="text-small mr-1">Поделиться этой статьей:</span>--}}
+                        {{--                        <span class="text-small mr-1">Поделиться этой статьей:</span>--}}
                         <div class="d-flex mx-2">
-                            <div
-                                    class="fb-like"
-                                    data-share="true"
-                                    data-width="450"
-                                    data-show-faces="true">
-                            </div>
-{{--                            <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.ampleev.com/blog-article&display=popup"--}}
-{{--                               class="btn btn-round btn-primary mx-1">--}}
-{{--                                <img class="icon icon-sm" src="assets/img/icons/social/facebook.svg"--}}
-{{--                                     alt="facebook social icon" data-inject-svg/>--}}
-{{--                            </a>--}}
+{{--                            <div--}}
+{{--                                    class="fb-like"--}}
+{{--                                    data-share="true"--}}
+{{--                                    data-width="450"--}}
+{{--                                    data-show-faces="true">--}}
+{{--                            </div>--}}
+                            <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.ampleev.com/blog-article&display=popup"
+                               class="btn btn-round btn-primary mx-1">
+                                <img class="icon icon-sm" src="assets/img/icons/social/facebook.svg"
+                                     alt="facebook social icon" data-inject-svg/>
+                            </a>
                             <a href="https://twitter.com/intent/tweet?text=Диаграммы%20сгорания%20в%20контексте%20SAFe%20-%20https://www.ampleev.com/blog-article"
                                class="btn btn-round btn-primary mx-1">
                                 <img class="icon icon-sm" src="assets/img/icons/social/twitter.svg"
