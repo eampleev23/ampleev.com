8dfaef7a0c3feec631148d84591d5d31bbd30f2d
Yevgeniy <e@mpleev.com>
2019-12-16 17:35:35 +0300

    fix

--- blog/app/ViewArticle.php
+++ blog/app/ViewArticle.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ViewArticle extends Model
 {
+
+    protected $fillable = [
+        'ip',
+        'user_id',
+    ];
+
     public function article()
     {
         return $this->belongsTo('App\Article');
