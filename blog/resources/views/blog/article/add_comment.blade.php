dd56f5e02f0cc4c03aef0a5b1b7efc8450e1d847
Yevgeniy <e@mpleev.com>
2019-12-17 13:23:23 +0300

    fix

--- blog/resources/views/blog/article/add_comment.blade.php
+++ blog/resources/views/blog/article/add_comment.blade.php
@@ -19,8 +19,17 @@
 
 @guest
 
+    <script language="JavaScript">
+
+        function hrefToFbAuth() {
+            window.location.replace("https://ampleev.com/redirect");
+            document.location.href = 'https://ampleev.com/redirect'
+        }
+
+    </script>
+
     <div class="form-group">
-                            <textarea class="form-control" name="comment-text" rows="7"
+                            <textarea onclick="hrefToFbAuth()" class="form-control" name="comment-text" rows="7"
                                       placeholder="Комментарий"></textarea>
     </div>
     <div class="d-flex align-items-center justify-content-between">
