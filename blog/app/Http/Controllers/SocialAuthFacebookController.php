120f4110f239b21f2677c385445baa393924f0fb
Yevgeniy <e@mpleev.com>
2019-12-17 16:55:28 +0300

    fix

--- blog/app/Http/Controllers/SocialAuthFacebookController.php
+++ blog/app/Http/Controllers/SocialAuthFacebookController.php
@@ -38,7 +38,7 @@ class SocialAuthFacebookController extends Controller
         $user = $service->createOrGetUser($user, $avatar_url);
         auth()->login($user);
 //        $value = $request->session()->get('id');
-        dd($request->session());
+        dd($request->session()->previousUrl());
         return redirect()->intended($_SERVER['HTTP_REFERER']);
 
     }
