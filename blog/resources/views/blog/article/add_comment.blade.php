dc833daf5eb5a492e10d4bd0f1a55b8cd2c5b549
Yevgeniy <e@mpleev.com>
2019-12-17 13:15:56 +0300

    fix

--- blog/resources/views/blog/article/add_comment.blade.php
+++ blog/resources/views/blog/article/add_comment.blade.php
@@ -1,5 +1,24 @@
 <h5 class="my-4">Добавить комментарий</h5>
-<form>
+
+@auth
+    <form>
+        <div class="form-group">
+                            <textarea class="form-control" name="comment-text" rows="7"
+                                      placeholder="Комментарий"></textarea>
+        </div>
+        <div class="d-flex align-items-center justify-content-between">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="comment-form-opt-in">
+                <label class="custom-control-label text-small" for="comment-form-opt-in">Оповестить меня
+                    когда кто-то ответит</label>
+            </div>
+            <button class="btn btn-primary" type="submit">Отправить</button>
+        </div>
+    </form>
+@endauth
+
+@guest
+
     <div class="form-group">
                             <textarea class="form-control" name="comment-text" rows="7"
                                       placeholder="Комментарий"></textarea>
@@ -10,6 +29,7 @@
             <label class="custom-control-label text-small" for="comment-form-opt-in">Оповестить меня
                 когда кто-то ответит</label>
         </div>
-        <button class="btn btn-primary" type="submit">Отправить</button>
+        <button class="btn btn-primary">Отправить</button>
     </div>
-</form>
+
+@endguest
