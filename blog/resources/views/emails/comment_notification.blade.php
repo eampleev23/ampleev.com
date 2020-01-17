3e27040d02810bb9be484d518aa5a84d59176d4c
Yevgeniy <e@mpleev.com>
2020-01-17 15:40:07 +0300

    fix

--- blog/resources/views/emails/comment_notification.blade.php
+++ blog/resources/views/emails/comment_notification.blade.php
@@ -381,13 +381,13 @@
                     <table role="presentation" border="0" cellpadding="0" cellspacing="0">
                         <tr>
                             <td class="content-block">
-                                <span class="apple-link">Company Inc, 3 Abbey Road, San Francisco CA 94102</span>
-                                <br> Don't like these emails? <a href="http://i.imgur.com/CScmqnj.gif">Unsubscribe</a>.
+                                <span class="apple-link">Ampleev.com, 50 Можайское шоссе, Москва</span>
+                                <br> Не хотите получать эти оповещения? <a href="#">Отпишитесь</a>.
                             </td>
                         </tr>
                         <tr>
                             <td class="content-block powered-by">
-                                Powered by <a href="http://htmlemail.io">HTMLemail</a>.
+                                Работает через сервис <a href="https://aws.amazon.com">AWS</a>.
                             </td>
                         </tr>
                     </table>
