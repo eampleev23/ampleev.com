15f5cd96363c772b7183678ad2b6c3db41e358c8
Yevgeniy <e@mpleev.com>
2020-01-18 18:18:06 +0300

    fix

--- blog/app/Http/Controllers/DocsController.php
+++ blog/app/Http/Controllers/DocsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class DocsController extends Controller
@@ -14,7 +15,8 @@ class DocsController extends Controller
 
     public function show_terms_of_use()
     {
-
-        return view('docs.terms_of_use');
+        $last_articles = Article::orderBy('created_at', 'desc')->where('type_article', '=',
+            "article")->limit(3)->get();
+        return view('docs.terms_of_use', compact('last_articles'));
     }
 }
