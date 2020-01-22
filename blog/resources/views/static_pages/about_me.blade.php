db56dfa07d0c94908ab122dce98fb0aa2b0695e7
Yevgeniy <e@mpleev.com>
2020-01-22 18:03:19 +0300

    fix

--- blog/resources/views/static_pages/about_me.blade.php
+++ blog/resources/views/static_pages/about_me.blade.php
@@ -67,12 +67,12 @@
             </div>
             <div class="row justify-content-center">
                 <div class="col-6 mb-3 col-lg-3 mb-lg-0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-                    <span class="display-4 text-primary d-block" data-countup="" data-start="1" data-end="13"
+                    <span class="display-4 text-primary d-block" data-countup="" data-start="0" data-end="13"
                           data-duration="3" data-grouping="true" data-suffix=" лет">13</span>
                     <span class="h6">Общий опыт в IT</span>
                 </div>
                 <div class="col-6 mb-3 col-lg-3 mb-lg-0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-                    <span class="display-4 text-primary d-block" data-countup="" data-start="1" data-end="9"
+                    <span class="display-4 text-primary d-block" data-countup="" data-start="0" data-end="9"
                           data-duration="3" data-grouping="true">9</span>
                     <span class="h6">Количество команд, с которыми работал</span>
                 </div>
@@ -82,7 +82,7 @@
                     <span class="h6">Количество успешно проведенных ретроспектив</span>
                 </div>
                 <div class="col-6 mb-3 col-lg-3 mb-lg-0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="400">
-                    <span class="display-4 text-primary d-block" data-countup="" data-start="1" data-end="7"
+                    <span class="display-4 text-primary d-block" data-countup="" data-start="-100" data-end="7"
                           data-duration="3" data-grouping="true" data-suffix=" из 10">7 из 10</span>
                     <span class="h6">Средняя обратная связь по фасилитации</span>
                 </div>
