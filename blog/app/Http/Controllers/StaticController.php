62443d49986822e511473c7a2bdf13fe68617713
Yevgeniy <e@mpleev.com>
2020-02-02 15:50:39 +0300

    fix

--- blog/app/Http/Controllers/StaticController.php
+++ blog/app/Http/Controllers/StaticController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use App\Layout;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
@@ -11,6 +12,8 @@ class StaticController extends Controller
     public function about_me()
     {
         $active_menu_item = 'Обо мне';
-        return view('static_pages.about_me', compact('active_menu_item'));
+        $last_articles = Article::orderBy('created_at', 'desc')->where('type_article', '=',
+            "article")->limit(3)->get();
+        return view('static_pages.about_me', compact('active_menu_item', 'last_articles'));
     }
 }
