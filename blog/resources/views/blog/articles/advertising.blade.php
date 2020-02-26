96a8906a036a4949fc896dc2068895e403887ebe
Yevgeniy <e@mpleev.com>
2020-02-26 16:17:23 +0300

    fix

--- blog/resources/views/blog/articles/advertising.blade.php
+++ blog/resources/views/blog/articles/advertising.blade.php
@@ -1,6 +1,60 @@
 <div class="sticky-top">
-    <a href="#"
-       class="bg-primary-3-alt rounded p-4 d-flex align-items-center justify-content-center min-vh-30">
-        <span class="text-small text-primary-3">Место для вашей рекламы</span>
-    </a>
+{{--    <a href="#"--}}
+{{--       class="bg-primary-3-alt rounded p-4 d-flex align-items-center justify-content-center min-vh-30">--}}
+{{--        <span class="text-small text-primary-3">Место для вашей рекламы</span>--}}
+{{--    </a>--}}
+
+
+
+
+    <div class="col-6 col-lg-3">
+        <h5>Последние статьи</h5>
+        <ul class="list-unstyled list-articles">
+            <li class="row row-tight">
+                <a href="https://ampleev.com/article_5_prostih_hakov_okruzhenia_razrabotchika_dlia_povishenia_proizvoditelnosti_chast_2" class="col-3">
+                    <img src="/assets/img/5haks2_main.jpg" alt="Image" class="rounded">
+                </a>
+                <div class="col">
+                    <a href="https://ampleev.com/article_5_prostih_hakov_okruzhenia_razrabotchika_dlia_povishenia_proizvoditelnosti_chast_2">
+                        <h6 class="mb-1">5 простых хаков окружения разработчика для повышения производительности. 2 часть.</h6>
+                    </a>
+                    <div class="d-flex text-small">
+                        <a href="#">Development</a>
+                        <span class="text-muted ml-1">сегодня в 15:51</span>
+                    </div>
+                </div>
+            </li>
+            <li class="row row-tight">
+                <a href="https://ampleev.com/article_5_prostih_hakov_okruzhenia_razrabotchika_dlia_povishenia_proizvoditelnosti_chast_1" class="col-3">
+                    <img src="/assets/img/5haks_main.jpg" alt="Image" class="rounded">
+                </a>
+                <div class="col">
+                    <a href="https://ampleev.com/article_5_prostih_hakov_okruzhenia_razrabotchika_dlia_povishenia_proizvoditelnosti_chast_1">
+                        <h6 class="mb-1">5 простых хаков окружения разработчика для повышения производительности. 1 часть.</h6>
+                    </a>
+                    <div class="d-flex text-small">
+                        <a href="#">Development</a>
+                        <span class="text-muted ml-1">вчера в 18:10</span>
+                    </div>
+                </div>
+            </li>
+            <li class="row row-tight">
+                <a href="https://ampleev.com/article_kak_zastavit_mikroservisi_obshatsia_chast2" class="col-3">
+                    <img src="/assets/img/microservices_p3.jpg" alt="Image" class="rounded">
+                </a>
+                <div class="col">
+                    <a href="https://ampleev.com/article_kak_zastavit_mikroservisi_obshatsia_chast2">
+                        <h6 class="mb-1">Как заставить микросервисы общаться? Часть 2.</h6>
+                    </a>
+                    <div class="d-flex text-small">
+                        <a href="#">Development</a>
+                        <span class="text-muted ml-1">20 февраля 2020 в 11:20</span>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+
+
 </div>
