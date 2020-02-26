4622a426720d6cc3b241cfb1643415979aa9af47
Yevgeniy <e@mpleev.com>
2020-02-26 16:10:31 +0300

    fix

--- blog/resources/views/blog/article/related_stories.blade.php
+++ blog/resources/views/blog/article/related_stories.blade.php
@@ -28,6 +28,29 @@
                         </div>
                     </a></noindex>
             </div>
+
+            <div class="col-md-6 col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="100">
+                <noindex><a rel="nofollow" href="{{$random_link->text_url}}"
+                            class="card card-body justify-content-between bg-primary text-light">
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="text-small d-flex">
+                                <div class="mr-2">
+                                    Ссылки
+                                </div>
+                                <span class="opacity-70">{{$random_link->get_nice_time_created()}}</span>
+                            </div>
+                            {{--                        <span class="badge bg-primary-alt text-primary">--}}
+                            {{--                  <img class="icon icon-sm bg-primary" src="assets/my_svg/Eye_view_views_enable_watch_1886932.svg"--}}
+                            {{--                       alt="heart interface icon" data-inject-svg/>{{$random_link->views_count}}--}}
+                            {{--                </span>--}}
+                        </div>
+                        <div>
+                            <h2>{{$random_link->title}}</h2>
+                            <span class="text-small opacity-70">{{$random_link->text_url}}</span>
+                        </div>
+                    </a></noindex>
+            </div>
+
             @for($i=0; $i < count($random_articles); $i++)
                 <div class="col-md-6 col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="200">
                     <div class="card">
