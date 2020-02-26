9951a0b33cfb655072ac60a76f8db4c128ba8404
Yevgeniy <e@mpleev.com>
2020-02-26 16:04:57 +0300

    fix

--- blog/resources/views/blog/articles/popular.blade.php
+++ blog/resources/views/blog/articles/popular.blade.php
@@ -3,9 +3,10 @@
     <ul class="list-unstyled list-articles">
         @for($i=0; $i < count($top_articles); $i++)
             <li class="row row-tight">
-                <a href="{{route('blog.show_article',$top_articles[$i]->text_url)}}" class="col-3">
-                    <img src="{{$top_articles[$i]->main_image_path}}" alt="Image" class="rounded">
-                </a>
+                <noindex><a rel="nofollow" href="{{route('blog.show_article',$top_articles[$i]->text_url)}}"
+                            class="col-3">
+                        <img src="{{$top_articles[$i]->main_image_path}}" alt="Image" class="rounded">
+                    </a></noindex>
                 <div class="col">
                     <a href="{{route('blog.show_article',$top_articles[$i]->text_url)}}">
                         <h6 class="mb-1">{{$top_articles[$i]->title}}</h6>
