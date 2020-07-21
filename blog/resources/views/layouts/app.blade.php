8291bf906fef60f33cb69122f3e3a55c7c83d5bc
Yevgeniy <e@mpleev.com>
2020-07-21 14:39:49 +0300

    fix

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
 
     <meta charset="utf-8">
 
-    <title>@yield('title') | Амплеев Евгений - Scrum Master / Full stack web developer</title>
+    <title>@yield('title') | Амплеев Евгений - Scrum Master / Full stack web developer. Персональный блог.</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="@yield('description')Персональный блог.">
 
