f31594d637b91ea1065a650f66d0c85413411206
Yevgeniy <e@mpleev.com>
2020-10-21 21:04:02 +0300

    fix

--- blog/resources/views/blog/articles/popular.blade.php
+++ blog/resources/views/blog/articles/popular.blade.php
@@ -12,7 +12,7 @@
                         <h6 class="mb-1">{{$top_articles[$i]->title}}</h6>
                     </a>
                     <div class="d-flex text-small">
-                        <a href="#">{{$top_articles[$i]->blog_section->title}}</a>
+                        <a href="{{route('blog.show_blog_section',$top_articles[$i]->blog_section->title)}}">{{$top_articles[$i]->blog_section->title}}</a>
                         <span class="text-muted ml-1">{{$top_articles[$i]->get_nice_time_created()}}</span>
                     </div>
                 </div>
