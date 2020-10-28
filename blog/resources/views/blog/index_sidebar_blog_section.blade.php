9873e7a5d4d2cb679c19a9a6fcc3d1c14e1496ca
Yevgeniy <e@mpleev.com>
2020-10-28 11:44:09 +0300

    fix

--- blog/resources/views/blog/index_sidebar_blog_section.blade.php
+++ blog/resources/views/blog/index_sidebar_blog_section.blade.php
@@ -25,6 +25,7 @@
 
     <section data-overlay>
         <div class="container">
+            <h1>Все посты раздела Development</h1>
             <div class="row mb-4">
                 @include('blog.articles.list_items')
                 <div class="col-md-4 col-lg-3 d-none d-md-block">
