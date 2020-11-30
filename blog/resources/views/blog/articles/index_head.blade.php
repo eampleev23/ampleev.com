28135b5acda44f9731d72f5e5218f36f4380a500
Yevgeniy <e@mpleev.com>
2020-11-30 16:08:44 +0300

    fix

--- blog/resources/views/blog/articles/index_head.blade.php
+++ blog/resources/views/blog/articles/index_head.blade.php
@@ -1,16 +1,38 @@
-<section class="has-divider text-light jarallax bg-dark" data-jarallax data-speed="0.5" data-overlay>
-    <img src="assets/img/article-99.jpg" alt="" class="jarallax-img opacity-50">
+{{--<section class="has-divider text-light jarallax bg-dark" data-jarallax data-speed="0.5" data-overlay>--}}
+{{--    <img src="assets/img/article-99.jpg" alt="" class="jarallax-img opacity-50">--}}
+{{--    <div class="container">--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-xl-5 col-lg-6 col-md-8">--}}
+{{--                <h1 class="display-4">Блог</h1>--}}
+{{--                <p class="lead mb-0">Планирую писать здесь обо всем, что меня волнует. В основном, это будет, как--}}
+{{--                    мне--}}
+{{--                    кажется, об Agile, но это не точно :)</p>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <div class="divider">--}}
+{{--        <img src="assets/img/dividers/divider-2.svg" alt="graphical divider" data-inject-svg/>--}}
+{{--    </div>--}}
+{{--</section>--}}
+
+
+<section class="has-divider text-light jarallax bg-dark" data-jarallax="" data-speed="0.5" data-overlay=""
+         style="padding-top: 73px !important;">
+
     <div class="container">
         <div class="row">
             <div class="col-xl-5 col-lg-6 col-md-8">
-                <h1 class="display-4">Блог</h1>
-                <p class="lead mb-0">Планирую писать здесь обо всем, что меня волнует. В основном, это будет, как
-                    мне
-                    кажется, об Agile, но это не точно :)</p>
+                <h1 class="display-4">Blog</h1>
+                <p class="lead mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
             </div>
         </div>
     </div>
     <div class="divider">
-        <img src="assets/img/dividers/divider-2.svg" alt="graphical divider" data-inject-svg/>
+        <img src="" alt="graphical divider" data-inject-svg="">
+    </div>
+    <div id="jarallax-container-0"
+         style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; pointer-events: none; z-index: -100;">
+        <img src="assets/img/article-9.jpg" alt="" class="jarallax-img opacity-50"
+             style="object-fit: cover; object-position: 50% 50%; max-width: none; position: fixed; top: 0px; left: 0px; width: 1425px; height: 406.82px; overflow: hidden; pointer-events: none; margin-top: -13.9102px; transform: translate3d(0px, 13.9102px, 0px);">
     </div>
 </section>
