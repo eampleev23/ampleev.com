64e32333e9e945daddbf1e1ecb801fa1f6bd2a0b
Yevgeniy <e@mpleev.com>
2020-12-23 22:56:57 +0300

    fix

--- blog/resources/views/blog/articles/index_head.blade.php
+++ blog/resources/views/blog/articles/index_head.blade.php
@@ -1,38 +1,106 @@
-<section class="has-divider text-light jarallax bg-dark" data-jarallax data-speed="0.5" data-overlay>
-    <img src="assets/img/article-99_2.jpg" alt="" class="jarallax-img opacity-50">
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-5 col-lg-6 col-md-8">
-                <h1 class="display-4">Блог</h1>
-                <p class="lead mb-0">Планирую писать здесь обо всем, что меня волнует. В основном, это будет, как
-                    мне
-                    кажется, об Agile, но это не точно :)</p>
-            </div>
-        </div>
-    </div>
-    <div class="divider">
-        <img src="assets/img/dividers/divider-2.svg" alt="graphical divider" data-inject-svg/>
-    </div>
-</section>
-
-
-{{--<section class="has-divider text-light jarallax bg-dark" data-jarallax="" data-speed="0.5" data-overlay=""--}}
-{{--         style="padding-top: 73px !important;">--}}
-
+{{--<section class="has-divider text-light jarallax bg-dark" data-jarallax data-speed="0.5" data-overlay>--}}
+{{--    <img src="assets/img/article-99_2.jpg" alt="" class="jarallax-img opacity-50">--}}
 {{--    <div class="container">--}}
 {{--        <div class="row">--}}
 {{--            <div class="col-xl-5 col-lg-6 col-md-8">--}}
-{{--                <h1 class="display-4">Blog</h1>--}}
-{{--                <p class="lead mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>--}}
+{{--                <h1 class="display-4">Блог</h1>--}}
+{{--                <p class="lead mb-0">Планирую писать здесь обо всем, что меня волнует. В основном, это будет, как--}}
+{{--                    мне--}}
+{{--                    кажется, об Agile, но это не точно :)</p>--}}
 {{--            </div>--}}
 {{--        </div>--}}
 {{--    </div>--}}
 {{--    <div class="divider">--}}
-{{--        <img src="" alt="graphical divider" data-inject-svg="">--}}
-{{--    </div>--}}
-{{--    <div id="jarallax-container-0"--}}
-{{--         style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; pointer-events: none; z-index: -100;">--}}
-{{--        <img src="assets/img/article-9.jpg" alt="" class="jarallax-img opacity-50"--}}
-{{--             style="object-fit: cover; object-position: 50% 50%; max-width: none; position: fixed; top: 0px; left: 0px; width: 1425px; height: 406.82px; overflow: hidden; pointer-events: none; margin-top: -13.9102px; transform: translate3d(0px, 13.9102px, 0px);">--}}
+{{--        <img src="assets/img/dividers/divider-2.svg" alt="graphical divider" data-inject-svg/>--}}
 {{--    </div>--}}
 {{--</section>--}}
+
+
+<section class="controls-inside controls-light p-0 flickity-enabled is-draggable"
+         data-flickity="{ &quot;imagesLoaded&quot;: true, &quot;wrapAround&quot;: true }" tabindex="0">
+
+
+    <div class="flickity-viewport" style="height: 265.297px; touch-action: pan-y;">
+        <div class="flickity-slider" style="left: 0px; transform: translateX(0%);">
+            <div class="carousel-cell bg-dark text-light overlay min-vh-70 d-flex flex-column justify-content-end is-selected"
+                 style="position: absolute; left: 0%;">
+                <img src="assets/img/article-9.jpg" alt="Image" class="bg-image opacity-80">
+                <div class="container">
+                    <div class="row pb-6">
+                        <div class="col">
+                            <a href="#">
+                                <span class="badge badge-primary">Business</span>
+                                <h1 class="mt-3">Interview with Leap founder Anna Perez</h1>
+                                <div class="d-flex align-items-center">
+                                    <img src="assets/img/avatars/female-3.jpg" alt="Avatar" class="avatar mr-2">
+                                    <div>
+                                        <div class="flex-shrink-0">by Abby Sims</div>
+                                        <div class="text-small text-muted">29th November</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell bg-dark text-light overlay min-vh-70 d-flex flex-column justify-content-end"
+                 aria-hidden="true" style="position: absolute; left: 100%;">
+                <img src="assets/img/article-10.jpg" alt="Image" class="bg-image opacity-80">
+                <div class="container">
+                    <div class="row pb-6">
+                        <div class="col">
+                            <a href="#">
+                                <span class="badge badge-primary">Design</span>
+                                <h1 class="mt-3">Productivity in the remote work era</h1>
+                                <div class="d-flex align-items-center">
+                                    <img src="assets/img/avatars/male-4.jpg" alt="Avatar" class="avatar mr-2">
+                                    <div>
+                                        <div class="flex-shrink-0">by Cedric</div>
+                                        <div class="text-small text-muted">27th November</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-cell bg-dark text-light overlay min-vh-70 d-flex flex-column justify-content-end"
+                 aria-hidden="true" style="position: absolute; left: 200%;">
+                <img src="assets/img/article-11.jpg" alt="Image" class="bg-image opacity-80">
+                <div class="container">
+                    <div class="row pb-6">
+                        <div class="col">
+                            <a href="#">
+                                <span class="badge badge-primary">Business</span>
+                                <h1 class="mt-3">A complete guide to approaching a VC</h1>
+                                <div class="d-flex align-items-center">
+                                    <img src="assets/img/avatars/male-1.jpg" alt="Avatar" class="avatar mr-2">
+                                    <div>
+                                        <div class="flex-shrink-0">by Ben Cameron</div>
+                                        <div class="text-small text-muted">23rd November</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="flickity-button flickity-prev-next-button previous" type="button" aria-label="Previous">
+        <svg class="flickity-button-icon" viewBox="0 0 100 100">
+            <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path>
+        </svg>
+    </button>
+    <button class="flickity-button flickity-prev-next-button next" type="button" aria-label="Next">
+        <svg class="flickity-button-icon" viewBox="0 0 100 100">
+            <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"
+                  transform="translate(100, 100) rotate(180) "></path>
+        </svg>
+    </button>
+    <ol class="flickity-page-dots">
+        <li class="dot is-selected" aria-label="Page dot 1" aria-current="step"></li>
+        <li class="dot" aria-label="Page dot 2"></li>
+        <li class="dot" aria-label="Page dot 3"></li>
+    </ol>
+</section>
