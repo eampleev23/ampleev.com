557001a74a89b1fdd704cd6c5aedb03126a1f1e4
Yevgeniy <e@mpleev.com>
2021-04-30 16:39:55 +0300

    fix

--- blog/app/Http/Controllers/SocialAuthFacebookController.php
+++ blog/app/Http/Controllers/SocialAuthFacebookController.php
@@ -16,9 +16,9 @@ class SocialAuthFacebookController extends Controller
      */
     public function redirect($whereback)
     {
-//        if ($whereback == 'add_comment') {
-//            $_SERVER['HTTP_REFERER'] = $_SERVER['HTTP_REFERER'] . '#add_comment';
-//        };
+        if ($whereback == 'add_comment') {
+            $_SERVER['HTTP_REFERER'] = $_SERVER['HTTP_REFERER'] . '#add_comment';
+        };
 //        dd($_SERVER['HTTP_REFERER']);
         return Socialite::driver('facebook')->redirect();
     }
