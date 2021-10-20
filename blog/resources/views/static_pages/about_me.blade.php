44eb935a65b26b9caa6fa5dfb3f0599a46a63a3b
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2021-10-20 10:39:59 +0300

    fix

--- blog/resources/views/static_pages/about_me.blade.php
+++ blog/resources/views/static_pages/about_me.blade.php
@@ -77,8 +77,8 @@
                     <span class="h6">Количество команд, с которыми работал</span>
                 </div>
                 <div class="col-6 mb-3 col-lg-3 mb-lg-0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
-                    <span class="display-4 text-primary d-block" data-countup="" data-start="0" data-end="25"
-                          data-duration="3" data-grouping="true">25</span>
+                    <span class="display-4 text-primary d-block" data-countup="" data-start="0" data-end="50"
+                          data-duration="3" data-grouping="true">50</span>
                     <span class="h6">Количество обученных Скрам&nbspМастеров</span>
                 </div>
                 <div class="col-6 mb-3 col-lg-3 mb-lg-0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="400">
