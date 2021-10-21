608d80de52036e1ba13a20952a357902b638cd43
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2021-10-21 15:53:45 +0300

    fix

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0071ff">
-    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
     <!-- Favicon -->
 
