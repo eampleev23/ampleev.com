12d563c1fa84d9c5e21dbf09ad362a8ca2751cf6
System Administrator <root@192.168.1.2>
2021-12-15 12:43:17 +0300

    fix

--- blog/resources/views/static_pages/about_me.blade.php
+++ blog/resources/views/static_pages/about_me.blade.php
@@ -164,7 +164,7 @@
                                 <h5 class="mb-0">Прошел тренинг <a
                                         href="/assets/pdf/your-management-3-0-certificate-2021-12-14.pdf">&#171;Advanced
                                         Management 3.0&#187;</a>
-                                    в <a href="https://management30.com"><img alt="Management 3.0"
+                                    в&nbsp<a href="https://management30.com"><img alt="Management 3.0"
                                                                               title="Management 3.0" width="87px"
                                                                               src="assets/img/m30-logo.png"
                                                                               style="
