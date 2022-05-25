5304507d48427af5db84fb404cacd5929f810813
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2022-05-25 14:31:17 +0300

    fix

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -26,13 +26,21 @@ Route::group([
 });
 
 Route::group([
+    'as' => 'static_pages.'
+],
+    function () {
+        Route::get('/about_me', 'StaticController@about_me')->name('about_me');
+    }
+);
+
+Route::group([
     'as' => 'blog.'
 ],
     function () {
         Route::get('/article_layout', 'BlogController@show_article_layout')->name('show_article_layout');
-//        Route::get('/', 'BlogController@show')->name('home');
+        Route::get('/', 'BlogController@show')->name('home');
 //        Route::get('/', 'StaticController@about_me')->name('static_pages.about_me');
-        Route::get('/', 'StaticController@about_me')->name('home');
+//        Route::get('/', 'StaticController@about_me')->name('home');
         Route::get('/sitemap.xml', 'BlogController@sitemap')->name('sitemap');
         Route::get('/article_{article_text_url}', 'BlogController@show_article')->name('show_article');
         Route::get('/blog_section_{blog_section_name}', 'BlogController@show_blog_section')->name('show_blog_section');
@@ -42,13 +50,13 @@ Route::group([
     }
 );
 
-Route::group([
-    'as' => 'static_pages.'
-],
-    function () {
-        Route::get('/about_me', 'StaticController@about_me')->name('about_me');
-    }
-);
+//Route::group([
+//    'as' => 'static_pages.'
+//],
+//    function () {
+//        Route::get('/about_me', 'StaticController@about_me')->name('about_me');
+//    }
+//);
 
 Route::group([
     'as' => 'utility.'
