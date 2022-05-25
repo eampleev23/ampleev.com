b1fadc5ea46f4cda3a88b551cce8b3d8a4b0d908
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2022-05-25 14:28:40 +0300

    fix

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -30,9 +30,9 @@ Route::group([
 ],
     function () {
         Route::get('/article_layout', 'BlogController@show_article_layout')->name('show_article_layout');
-        Route::get('/', 'BlogController@show')->name('home');
+//        Route::get('/', 'BlogController@show')->name('home');
 //        Route::get('/', 'StaticController@about_me')->name('static_pages.about_me');
-//        Route::get('/', 'StaticController@about_me')->name('home');
+        Route::get('/', 'StaticController@about_me')->name('home');
         Route::get('/sitemap.xml', 'BlogController@sitemap')->name('sitemap');
         Route::get('/article_{article_text_url}', 'BlogController@show_article')->name('show_article');
         Route::get('/blog_section_{blog_section_name}', 'BlogController@show_blog_section')->name('show_blog_section');
@@ -82,10 +82,10 @@ Route::group([
 
 
 //Route::get('/', 'IndexController@show')->name('main');
-//Route::get('/blog', 'BlogController@show')->name('test');
+//Route::get('/blog', 'BlogController@show')->name('test'); sdf
 
 
-//facebook auth_
+//facebook auth
 Route::get('/redirect-{whereback}', 'SocialAuthFacebookController@redirect');
 Route::get('/callback', 'SocialAuthFacebookController@callback');
 
