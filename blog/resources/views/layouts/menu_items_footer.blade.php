dc4dd567092e117859af900141fb2ed098b700ba
System Administrator <root@192.168.1.3>
2024-06-10 18:19:42 +0300

    fix

--- blog/resources/views/layouts/menu_items_footer.blade.php
+++ blog/resources/views/layouts/menu_items_footer.blade.php
@@ -6,26 +6,26 @@
             <li class="nav-item">
                 <a href="#" class="nav-link active">Обо мне</a>
             </li>
-            <li class="nav-item">
-                <a href="{{route('blog.blog')}}" class="nav-link">Блог</a>
-            </li>
+{{--            <li class="nav-item">--}}
+{{--                <a href="{{route('blog.blog')}}" class="nav-link">Блог</a>--}}
+{{--            </li>--}}
             <li class="nav-item">
                 <a href="{{route('docs.terms_of_use')}}" class="nav-link">Правила</a>
             </li>
             @guest
-                <li class="nav-item">
-                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>--}}
+{{--                </li>--}}
             @endguest
             @auth
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">Выйти</a>
-                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"
-                          style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">Выйти</a>--}}
+{{--                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"--}}
+{{--                          style="display: none;">--}}
+{{--                        {{ csrf_field() }}--}}
+{{--                    </form>--}}
+{{--                </li>--}}
             @endauth
         </ul>
 
@@ -35,26 +35,26 @@
             <li class="nav-item">
                 <a href="{{route('static_pages.about_me')}}" class="nav-link">Обо мне</a>
             </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link active">Блог</a>
-            </li>
+{{--            <li class="nav-item">--}}
+{{--                <a href="#" class="nav-link active">Блог</a>--}}
+{{--            </li>--}}
             <li class="nav-item">
                 <a href="{{route('docs.terms_of_use')}}" class="nav-link">Правила</a>
             </li>
             @guest
-                <li class="nav-item">
-                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>--}}
+{{--                </li>--}}
             @endguest
             @auth
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">Выйти</a>
-                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"
-                          style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">Выйти</a>--}}
+{{--                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"--}}
+{{--                          style="display: none;">--}}
+{{--                        {{ csrf_field() }}--}}
+{{--                    </form>--}}
+{{--                </li>--}}
             @endauth
         </ul>
         @break
@@ -64,26 +64,26 @@
             <li class="nav-item">
                 <a href="{{route('static_pages.about_me')}}" class="nav-link">Обо мне</a>
             </li>
-            <li class="nav-item">
-                <a href="{{route('blog.blog')}}" class="nav-link active">Блог</a>
-            </li>
+{{--            <li class="nav-item">--}}
+{{--                <a href="{{route('blog.blog')}}" class="nav-link active">Блог</a>--}}
+{{--            </li>--}}
             <li class="nav-item">
                 <a href="{{route('docs.terms_of_use')}}" class="nav-link">Правила</a>
             </li>
             @guest
-                <li class="nav-item">
-                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>--}}
+{{--                </li>--}}
             @endguest
             @auth
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">Выйти</a>
-                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"
-                          style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">Выйти</a>--}}
+{{--                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"--}}
+{{--                          style="display: none;">--}}
+{{--                        {{ csrf_field() }}--}}
+{{--                    </form>--}}
+{{--                </li>--}}
             @endauth
         </ul>
         @break
@@ -93,26 +93,26 @@
             <li class="nav-item">
                 <a href="{{route('static_pages.about_me')}}" class="nav-link">Обо мне</a>
             </li>
-            <li class="nav-item">
-                <a href="{{route('blog.blog')}}" class="nav-link">Блог</a>
-            </li>
+{{--            <li class="nav-item">--}}
+{{--                <a href="{{route('blog.blog')}}" class="nav-link">Блог</a>--}}
+{{--            </li>--}}
             <li class="nav-item">
                 <a href="#" class="nav-link active">Правила</a>
             </li>
             @guest
-                <li class="nav-item">
-                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>--}}
+{{--                </li>--}}
             @endguest
             @auth
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">Выйти</a>
-                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"
-                          style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">Выйти</a>--}}
+{{--                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"--}}
+{{--                          style="display: none;">--}}
+{{--                        {{ csrf_field() }}--}}
+{{--                    </form>--}}
+{{--                </li>--}}
             @endauth
         </ul>
         @break
@@ -122,26 +122,26 @@
             <li class="nav-item">
                 <a href="{{route('static_pages.about_me')}}" class="nav-link">Обо мне</a>
             </li>
-            <li class="nav-item">
-                <a href="{{route('blog.blog')}}" class="nav-link">Блог</a>
-            </li>
+{{--            <li class="nav-item">--}}
+{{--                <a href="{{route('blog.blog')}}" class="nav-link">Блог</a>--}}
+{{--            </li>--}}
             <li class="nav-item">
                 <a href="{{route('docs.terms_of_use')}}" class="nav-link">Правила</a>
             </li>
             @guest
-                <li class="nav-item">
-                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a href="/redirect-default" class="nav-link">Авторизоваться через Facebook</a>--}}
+{{--                </li>--}}
             @endguest
             @auth
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">Выйти</a>
-                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"
-                          style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">Выйти</a>--}}
+{{--                    <form id="logout-form" action="{{ url('/logout') }}" method="POST"--}}
+{{--                          style="display: none;">--}}
+{{--                        {{ csrf_field() }}--}}
+{{--                    </form>--}}
+{{--                </li>--}}
             @endauth
         </ul>
     @endswitch
