4dab9dbd6353f4affb53d275ffc0c5635e4bdb3f
Евгений Амплеев <eampleev@MacBook-Pro-Evgenij-2.local>
2024-12-12 11:10:02 +0300

    fix br

--- blog/resources/views/static_pages/about_me.blade.php
+++ blog/resources/views/static_pages/about_me.blade.php
@@ -275,7 +275,7 @@
                             <div>
                                 <span class="text-small text-muted">Февраль 2024</span>
                                 <h5 class="mb-0" style="margin-top: -30px;margin-left: -16px;"> &nbsp;&nbsp;&nbsp;Исполнительный
-                                    директор в <br/>
+                                    директор в
                                     <a href="https://www.gazprombank.ru">
                                         <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg"
                                              xmlns:xlink="http://www.w3.org/1999/xlink"
