67ca0dfdbdffef58b6b5e19c4d4964ef69ff7da4
Евгений Амплеев <eampleev@MacBook-Pro-Evgenij-2.local>
2024-12-26 11:51:47 +0300

    blog return

--- blog/resources/views/blog/article/related_stories.blade.php
+++ blog/resources/views/blog/article/related_stories.blade.php
@@ -29,42 +29,42 @@
                 </a>
             </div>
 
-            @for($i=0; $i < count($random_articles); $i++)
-                <div class="col-md-6 col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="200">
-                    <div class="card">
-                        <a href="{{route('blog.show_article',$random_articles[$i]->text_url)}}">
-                            <img src="{{$random_articles[$i]->main_image_path}}" alt="Image" class="card-img-top">
-                        </a>
-                        <div class="card-body d-flex flex-column">
-                            <div class="d-flex justify-content-between mb-3">
-                                <div class="text-small d-flex">
-                                    <div class="mr-2">
-                                        <a href="#">{{$random_articles[$i]->blog_section->title}}</a>
-                                    </div>
-                                    <span class="text-muted">{{$random_articles[$i]->get_nice_time_created()}}</span>
-                                </div>
-                                <span class="badge bg-primary-alt text-primary">
-                    <img class="icon icon-sm bg-primary" src="assets/my_svg/Eye_view_views_enable_watch_1886932.svg"
-                         alt="heart interface icon" data-inject-svg/>{{$random_articles[$i]->views_count}}
-                  </span>
-                            </div>
-                            <a href="{{route('blog.show_article',$random_articles[$i]->text_url)}}">
-                                <h4>{{$random_articles[$i]->title}}</h4>
-                            </a>
-                            <p class="flex-grow-1">
-                                {{$random_articles[$i]->seo_description}}
-                            </p>
-                            <div class="d-flex align-items-center mt-3">
-                                <img src="{{env('APP_URL').$random_articles[$i]->user->avatar_path}}" alt="Image"
-                                     class="avatar avatar-sm">
-                                <div class="ml-1">
-                                    <span class="text-small">{{$random_articles[$i]->user->name}}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endfor
+{{--            @for($i=0; $i < count($random_articles); $i++)--}}
+{{--                <div class="col-md-6 col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="200">--}}
+{{--                    <div class="card">--}}
+{{--                        <a href="{{route('blog.show_article',$random_articles[$i]->text_url)}}">--}}
+{{--                            <img src="{{$random_articles[$i]->main_image_path}}" alt="Image" class="card-img-top">--}}
+{{--                        </a>--}}
+{{--                        <div class="card-body d-flex flex-column">--}}
+{{--                            <div class="d-flex justify-content-between mb-3">--}}
+{{--                                <div class="text-small d-flex">--}}
+{{--                                    <div class="mr-2">--}}
+{{--                                        <a href="#">{{$random_articles[$i]->blog_section->title}}</a>--}}
+{{--                                    </div>--}}
+{{--                                    <span class="text-muted">{{$random_articles[$i]->get_nice_time_created()}}</span>--}}
+{{--                                </div>--}}
+{{--                                <span class="badge bg-primary-alt text-primary">--}}
+{{--                    <img class="icon icon-sm bg-primary" src="assets/my_svg/Eye_view_views_enable_watch_1886932.svg"--}}
+{{--                         alt="heart interface icon" data-inject-svg/>{{$random_articles[$i]->views_count}}--}}
+{{--                  </span>--}}
+{{--                            </div>--}}
+{{--                            <a href="{{route('blog.show_article',$random_articles[$i]->text_url)}}">--}}
+{{--                                <h4>{{$random_articles[$i]->title}}</h4>--}}
+{{--                            </a>--}}
+{{--                            <p class="flex-grow-1">--}}
+{{--                                {{$random_articles[$i]->seo_description}}--}}
+{{--                            </p>--}}
+{{--                            <div class="d-flex align-items-center mt-3">--}}
+{{--                                <img src="{{env('APP_URL').$random_articles[$i]->user->avatar_path}}" alt="Image"--}}
+{{--                                     class="avatar avatar-sm">--}}
+{{--                                <div class="ml-1">--}}
+{{--                                    <span class="text-small">{{$random_articles[$i]->user->name}}</span>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            @endfor--}}
         </div>
     </div>
 </section>
