e4d8aa9494c5be3302e7b06d7a00ff141583c864
Евгений Амплеев <eampleev@MacBook-Pro-Evgenij-2.local>
2025-03-24 14:37:48 +0300

    add migration content

--- blog/resources/views/test/article.blade.php
+++ blog/resources/views/test/article.blade.php
@@ -336,7 +336,8 @@ $go test -v ./...
 PASS
 ok      bitbucket.org/ampleevee/examples.git/internal/books      0.273s
 
-------------------- Go Version: go 1.22.0
+
+-------- Go Version: go 1.22.0
                         </code></pre>
 
                 </article>
