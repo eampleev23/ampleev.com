1b3fc446bb2376a480228bb5cfa686e6ca2ae90b
Евгений Амплеев <eampleev@MacBook-Pro-Evgenij-2.local>
2025-03-25 10:35:08 +0300

    static article

--- blog/app/Http/Controllers/BlogController.php
+++ blog/app/Http/Controllers/BlogController.php
@@ -30,7 +30,7 @@ class BlogController extends Controller
 //            '=', '1')->get();
         $top_articles = Article::orderBy('views_count', 'desc')->where('confirmed',
             '=', '1')->where('type_article', '=',
-            "article")->limit(10)->get();
+            "article")->limit(11)->get();
         $last_articles = Article::orderBy('views_count', 'desc')->where('confirmed',
             '=', '1')->where('type_article', '=',
             "article")->limit(2)->get();
