b75062646a23e913ce211cd809c7dfb338893f9d
Евгений Амплеев <eampleev@MacBook-Pro-Evgenij-2.local>
2025-03-27 13:41:13 +0300

    static x

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -173,9 +173,20 @@
                         {{--                                 alt="twitter social icon" data-inject-svg/>--}}
                         {{--                        </a>--}}
                         <a href="https://x.com/ampleevE"
-                           class="nav-link fix_padding_social">
+                           class="nav-link fix_padding_social" style="margin-left: -17px;">
                             <img class="icon undefined" src="assets/img/x-social.svg"
                                  alt="x social icon" data-inject-svg/>
+                        </a>
+                        <a href="https://x.com/ampleevE" class="nav-link fix_padding_social">
+                            <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision"
+                                 text-rendering="geometricPrecision" image-rendering="optimizeQuality"
+                                 fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 462.799"
+                                 class="injected-svg icon undefined" data-src="assets/img/x-social.svg"
+                                 xmlns:xlink="http://www.w3.org/1999/xlink" style="
+">
+                                <path fill-rule="nonzero"
+                                      d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"></path>
+                            </svg>
                         </a>
                     </li>
                     {{--                    <li class="nav-item">--}}
