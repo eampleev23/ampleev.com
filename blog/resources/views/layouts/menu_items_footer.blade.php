0dbee27b9e1f4d04bb884a31efacfdaa8856a14b
ampleev.com <e@mpleev.com>
2025-11-14 09:40:21 +0300

    add contacts to cv

--- blog/resources/views/layouts/menu_items_footer.blade.php
+++ blog/resources/views/layouts/menu_items_footer.blade.php
@@ -18,53 +18,53 @@
             {{--                </li>--}}
             <li class="nav-item">
 {{--                <a href="/redirect-vk" class="nav-link">Авторизоваться через VK</a>--}}
-                <div>
-                    <script src="https://unpkg.com/@vkid/sdk@<3.0.0/dist-sdk/umd/index.js"></script>
-                    <script type="text/javascript">
-                        if ('VKIDSDK' in window) {
-                            const VKID = window.VKIDSDK;
+{{--                <div>--}}
+{{--                    <script src="https://unpkg.com/@vkid/sdk@<3.0.0/dist-sdk/umd/index.js"></script>--}}
+{{--                    <script type="text/javascript">--}}
+{{--                        if ('VKIDSDK' in window) {--}}
+{{--                            const VKID = window.VKIDSDK;--}}
 
-                            VKID.Config.init({
-                                app: 53261431,
-                                redirectUrl: 'https://ampleev.com/redirect-vk',
-                                responseMode: VKID.ConfigResponseMode.Callback,
-                                source: VKID.ConfigSource.LOWCODE,
-                                scope: '', // Заполните нужными доступами по необходимости
-                            });
+{{--                            VKID.Config.init({--}}
+{{--                                app: 53261431,--}}
+{{--                                redirectUrl: 'https://ampleev.com/redirect-vk',--}}
+{{--                                responseMode: VKID.ConfigResponseMode.Callback,--}}
+{{--                                source: VKID.ConfigSource.LOWCODE,--}}
+{{--                                scope: '', // Заполните нужными доступами по необходимости--}}
+{{--                            });--}}
 
-                            const oneTap = new VKID.OneTap();
+{{--                            const oneTap = new VKID.OneTap();--}}
 
-                            oneTap.render({
-                                container: document.currentScript.parentElement,
-                                showAlternativeLogin: true,
-                                styles: {
-                                    height: 38
-                                },
-                                oauthList: [
-                                    'ok_ru',
-                                    'mail_ru'
-                                ]
-                            })
-                                .on(VKID.WidgetEvents.ERROR, vkidOnError)
-                                .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, function (payload) {
-                                    const code = payload.code;
-                                    const deviceId = payload.device_id;
+{{--                            oneTap.render({--}}
+{{--                                container: document.currentScript.parentElement,--}}
+{{--                                showAlternativeLogin: true,--}}
+{{--                                styles: {--}}
+{{--                                    height: 38--}}
+{{--                                },--}}
+{{--                                oauthList: [--}}
+{{--                                    'ok_ru',--}}
+{{--                                    'mail_ru'--}}
+{{--                                ]--}}
+{{--                            })--}}
+{{--                                .on(VKID.WidgetEvents.ERROR, vkidOnError)--}}
+{{--                                .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, function (payload) {--}}
+{{--                                    const code = payload.code;--}}
+{{--                                    const deviceId = payload.device_id;--}}
 
-                                    VKID.Auth.exchangeCode(code, deviceId)
-                                        .then(vkidOnSuccess)
-                                        .catch(vkidOnError);
-                                });
+{{--                                    VKID.Auth.exchangeCode(code, deviceId)--}}
+{{--                                        .then(vkidOnSuccess)--}}
+{{--                                        .catch(vkidOnError);--}}
+{{--                                });--}}
 
-                            function vkidOnSuccess(data) {
-                                // Обработка полученного результата
-                            }
+{{--                            function vkidOnSuccess(data) {--}}
+{{--                                // Обработка полученного результата--}}
+{{--                            }--}}
 
-                            function vkidOnError(error) {
-                                // Обработка ошибки
-                            }
-                        }
-                    </script>
-                </div>
+{{--                            function vkidOnError(error) {--}}
+{{--                                // Обработка ошибки--}}
+{{--                            }--}}
+{{--                        }--}}
+{{--                    </script>--}}
+{{--                </div>--}}
             </li>
         @endguest
         @auth
