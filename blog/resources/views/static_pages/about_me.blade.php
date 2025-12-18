09ba117f1a39b9982aa5bd071e1498d0c82c14ec
ampleev.com <e@mpleev.com>
2025-12-18 18:29:36 +0300

    update about me

--- blog/resources/views/static_pages/about_me.blade.php
+++ blog/resources/views/static_pages/about_me.blade.php
@@ -69,36 +69,63 @@
                         <div class="col-xl-8 col-lg-10">
                             <span class="badge badge-primary">Коротко</span>
                             <div class="my-3">
-                                {{--                                <span class="h1">Обо мне</span>--}}
                                 <h1>Обо мне</h1>
                             </div>
 
-                            <p class="lead"><b>IT-эксперт</b> с 19-летним опытом в <code>FinTech</code>,
+                            <p class="lead">IT-эксперт с 19-летним опытом в <code><b>FinTech</b></code>,
                                 крупном
-                                цифровом бизнесе и стартапах. Строю и трансформирую инженерные организации, соединяя
+                                цифровом бизнесе и стартапах. Строю и трансформирую инженерные организации, с фокусом
+                                на ключевые бизнес-метрики: <code><b>Retention</b></code>, <code><b>LTV</b></code>,
+                                <code><b>P&L</b></code>, <code><b>TTM</b></code>. Соединяю
                                 глубокую
-                                продуктовую экспертизу (управление командами, запуск <code>CRM</code>, <code>мобильных
-                                    приложений</code>) с
-                                системной работой над процессами (<code>Agile</code>-трансформации, разработка
-                                методологий).</p>
+                                продуктовую экспертизу (управление командами, запуск CRM, мобильных
+                                приложений) с
+                                системной работой над процессами (<code><b>Agile</b></code>-трансформации, разработка
+                                методологий) для достижения измеримых результатов.</p>
                             <br/>
 
-                            <p class="lead">Сохраняю актуальную техническую экспертизу (<code>Go</code>,
-                                <code>Swift</code>, <code>React</code>, <code>CI/CD</code>) и активно
-                                применяю современные <code>AI/ML-технологии</code> в рабочем стеке.</p>
+                            <p class="lead">Сохраняю актуальную техническую экспертизу (<code><b>Go</b></code>,
+                                <code><b>Swift</b></code>, <code><b>JS</b></code>, <code><b>CI/CD</b></code>) и активно
+                                применяю современные <code><b>AI/ML</b></code>-технологии в рабочем стеке, нацеливая
+                                разработку
+                                на оптимизацию бизнес-показателей.</p>
                             <br/>
                             <p><b>Cертификаты:</b></p>
                             <ul>
-                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Advanced Go Developer в Yandex Practicum®" href="/assets/pdf/ampleev_em_cert_advanced_golang_20242GP00104.pdf">Advanced Go Developer в Yandex Practicum®</a></li>
-                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Advanced Management 3.0 в M 3.0®" href="/assets/pdf/your-management-3-0-certificate-2021-12-14.pdf">Advanced Management 3.0 в M 3.0®</a></li>
-{{--                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс SAFe for teams в Ингосстрах®" href="/assets/pdf/safe_teams_compressed.pdf">SAFe for teams в Ингосстрах®</a></li>--}}
-{{--                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс SAFe for teams в Ингосстрах®" href="/assets/pdf/safe_sm_compressed.pdf">SAFe for sm в Ингосстрах®</a></li>--}}
-                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс React JS в @mail.ru group®" href="/assets/pdf/react.pdf">React JS в @mail.ru group®</a></li>
-                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Linux в @mail.ru group®" href="/assets/pdf/linux_level_1.pdf">Linux в @mail.ru group®</a></li>
-                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Professional Backend Developer в @mail.ru group®" href="/assets/pdf/php_yii2.pdf">Professional Backend Developer в @mail.ru group®</a></li>
-                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Data Bases - SQL в @mail.ru group® в @mail.ru group®" href="/assets/pdf/db_base_video.pdf">Data Bases - SQL в @mail.ru group®</a></li>
-                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Git в @mail.ru group® в @mail.ru group®" href="/assets/pdf/git.pdf">Git в @mail.ru group®</a></li>
-                                <li><a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Web design в @mail.ru group®" href="/assets/pdf/web_design_base.pdf">Web design в @mail.ru group®</a></li>
+                                <li>
+                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Advanced Go Developer в Yandex Practicum®"
+                                       href="/assets/pdf/ampleev_em_cert_advanced_golang_20242GP00104.pdf">Advanced Go
+                                        Developer в Yandex Practicum®</a></li>
+                                <li>
+                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Advanced Management 3.0 в M 3.0®"
+                                       href="/assets/pdf/your-management-3-0-certificate-2021-12-14.pdf">Advanced
+                                        Management 3.0 в M 3.0®</a></li>
+                                <li>
+                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс React JS в @mail.ru group®"
+                                       href="/assets/pdf/react.pdf">React JS в @mail.ru group®</a></li>
+                                <li>
+                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Linux в @mail.ru group®"
+                                       href="/assets/pdf/linux_level_1.pdf">Linux в @mail.ru group®</a></li>
+                                <li>
+                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Professional Backend Developer в @mail.ru group®"
+                                       href="/assets/pdf/php_yii2.pdf">Professional Backend Developer в @mail.ru
+                                        group®</a></li>
+                                <li>
+                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Data Bases - SQL в @mail.ru group® в @mail.ru group®"
+                                       href="/assets/pdf/db_base_video.pdf">Data Bases - SQL в @mail.ru group®</a></li>
+                                <li>
+                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Git в @mail.ru group® в @mail.ru group®"
+                                       href="/assets/pdf/git.pdf">Git в @mail.ru group®</a></li>
+                                <li>
+                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс Web design в @mail.ru group®"
+                                       href="/assets/pdf/web_design_base.pdf">Web design в @mail.ru group®</a></li>
+                                {{--                                <li>--}}
+                                {{--                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс SAFe for teams в Ингосстрах®"--}}
+                                {{--                                       href="/assets/pdf/safe_teams_compressed.pdf">SAFe for teams в Ингосстрах®</a>--}}
+                                {{--                                </li>--}}
+                                {{--                                <li>--}}
+                                {{--                                    <a title="Официальный сертификат Амплеева Евгения Михайловича, подтверждающий, что он прошел курс SAFe for teams в Ингосстрах®"--}}
+                                {{--                                       href="/assets/pdf/safe_sm_compressed.pdf">SAFe for sm в Ингосстрах®</a></li>--}}
                             </ul>
                         </div>
                     </div>
