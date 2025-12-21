49914a7e285357c15d1ab379727e1abc70013e53
ampleev.com <e@mpleev.com>
2025-12-21 11:13:31 +0300

    2025-12-21 11:13:31

--- blog/app/Providers/EventServiceProvider.php
+++ blog/app/Providers/EventServiceProvider.php
@@ -19,7 +19,7 @@ class EventServiceProvider extends ServiceProvider
             SendEmailVerificationNotification::class,
         ],
         \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-            \SocialiteProviders\Yandex\YandexExtendSocialite::class . '@handle',
+            \SocialiteProviders\Yandex\YandexExtendSocialite::class,
         ],
     ];
 
