f450be553480fbc03d97bad8406265ce44f15b51
ampleev.com <e@mpleev.com>
2025-12-21 11:08:45 +0300

    Fix Yandex provider registration

--- blog/app/Providers/AppServiceProvider.php
+++ blog/app/Providers/AppServiceProvider.php
@@ -4,10 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Event;
-use Laravel\Socialite\Facades\Socialite;
-use SocialiteProviders\Manager\SocialiteWasCalled;
-use SocialiteProviders\Yandex\Provider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,30 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
-        // Регистрируем провайдер Yandex напрямую через Socialite
-        $this->app->booted(function () {
-            \Log::info('App booted, registering Yandex provider directly');
-            
-            // Регистрируем через событие (для совместимости)
-            Event::listen(SocialiteWasCalled::class, function (SocialiteWasCalled $event) {
-                \Log::info('SocialiteWasCalled event fired');
-                $event->extendSocialite('yandex', Provider::class);
-                \Log::info('Yandex provider extended via event');
-            });
-            
-            // Регистрируем напрямую через Socialite::extend()
-            try {
-                $config = config('services.yandex');
-                Socialite::extend('yandex', function ($app) use ($config) {
-                    return Socialite::buildProvider(Provider::class, $config);
-                });
-                \Log::info('Yandex provider extended directly via Socialite::extend()');
-            } catch (\Exception $e) {
-                \Log::error('Failed to register Yandex provider directly: ' . $e->getMessage());
-            }
-            
-            \Log::info('Yandex provider registration completed');
-        });
     }
 }
